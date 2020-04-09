d3209e8972703e33ead9e235d77523f95766e285
crvernon <chrisrvernon@gmail.com>
2020-04-09 17:50:30 -0400

    working model output tests

---
+++ im3py/tests/test_model.py
@@ -0,0 +1,121 @@
+"""Tests for the configuration reader functionality.
+
+:author:   <developer names>
+:email:    <developer emails>
+
+License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
+
+"""
+
+import pkg_resources
+import unittest
+
+from im3py.model import Model
+
+
+class TestReadConfig(unittest.TestCase):
+    """Tests for the `ReadConfig` class that reads the input configuration from the user."""
+
+    # test config YAML file
+    CONFIG_YAML = pkg_resources.resource_filename('im3py', 'tests/data/comp_data/config.yml')
+
+    # expected attribute values
+    OUTPUT_DIR = "data/outputs"
+    START_YEAR = 2015
+    THROUGH_YEAR = 2020
+    TIME_STEP = 1
+    ALPHA_URBAN = 2.0
+    ALPHA_RURAL = 0.08
+    BETA_URBAN = 1.78
+    BETA_RURAL = 1.42
+
+    # expected attribute types
+    OUTPUT_DIR_TYPE = str
+    START_YEAR_TYPE = int
+    THROUGH_YEAR_TYPE = int
+    TIME_STEP_TYPE = int
+    ALPHA_URBAN_TYPE = float
+    ALPHA_RURAL_TYPE = float
+    BETA_URBAN_TYPE = float
+    BETA_RURAL_TYPE = float
+
+    def test_model_outputs(self):
+        """Ensure model outputs are what is expected."""
+
+        run = Model(output_directory=TestReadConfig.OUTPUT_DIR,
+                         start_year=TestReadConfig.START_YEAR,
+                         through_year=TestReadConfig.THROUGH_YEAR,
+                         time_step=TestReadConfig.TIME_STEP,
+                         alpha_urban= TestReadConfig.ALPHA_URBAN,
+                         alpha_rural=TestReadConfig.ALPHA_RURAL,
+                         beta_urban=TestReadConfig.BETA_URBAN,
+                         beta_rural=TestReadConfig.BETA_RURAL)
+
+        run.run_all_steps()
+
+        
+
+    # def test_instance_attributes_config(self):
+    #     """Test that the correct instance attribute values are generated from the config file read."""
+    #
+    #     # read configuration from file
+    #     run = Model(config_file=TestReadConfig.CONFIG_YAML)
+    #
+    #     # check value equality for each variable with expected
+    #     self.check_values(run)
+    #
+    #     # check type equality for each variable with expected
+    #     self.check_types(run)
+    #
+    # def test_instance_attributes_params(self):
+    #     """Test that the correct instance attribute values are generated from parameters."""
+    #
+    #     # read configuration from parameters
+    #     run = Model(output_directory=TestReadConfig.OUTPUT_DIR,
+    #                      start_year=TestReadConfig.START_YEAR,
+    #                      through_year=TestReadConfig.THROUGH_YEAR,
+    #                      time_step=TestReadConfig.TIME_STEP,
+    #                      alpha_urban= TestReadConfig.ALPHA_URBAN,
+    #                      alpha_rural=TestReadConfig.ALPHA_RURAL,
+    #                      beta_urban=TestReadConfig.BETA_URBAN,
+    #                      beta_rural=TestReadConfig.BETA_RURAL)
+    #
+    #     # check value equality for each variable with expected
+    #     self.check_values(run)
+    #
+    #     # check type equality for each variable with expected
+    #     self.check_types(run)
+
+    # def check_values(self, cfg):
+    #     """Check values of each configuration attribute against expected.
+    #
+    #     :param cfg:                     Configuration object
+    #
+    #     """
+    #     self.assertEqual(cfg.output_directory, TestReadConfig.OUTPUT_DIR)
+    #     self.assertEqual(cfg.start_year, TestReadConfig.START_YEAR)
+    #     self.assertEqual(cfg.through_year, TestReadConfig.THROUGH_YEAR)
+    #     self.assertEqual(cfg.time_step, TestReadConfig.TIME_STEP)
+    #     self.assertEqual(cfg.alpha_urban, TestReadConfig.ALPHA_URBAN)
+    #     self.assertEqual(cfg.alpha_rural, TestReadConfig.ALPHA_RURAL)
+    #     self.assertEqual(cfg.beta_urban, TestReadConfig.BETA_URBAN)
+    #     self.assertEqual(cfg.beta_rural, TestReadConfig.BETA_RURAL)
+    #
+    # def check_types(self, cfg):
+    #     """Check types of each configuration attribute against expected.
+    #
+    #     :param cfg:                     Configuration object
+    #
+    #     """
+    #     self.assertEqual(type(cfg.output_directory), TestReadConfig.OUTPUT_DIR_TYPE)
+    #     self.assertEqual(type(cfg.start_year), TestReadConfig.START_YEAR_TYPE)
+    #     self.assertEqual(type(cfg.through_year), TestReadConfig.THROUGH_YEAR_TYPE)
+    #     self.assertEqual(type(cfg.time_step), TestReadConfig.TIME_STEP_TYPE)
+    #     self.assertEqual(type(cfg.alpha_urban), TestReadConfig.ALPHA_URBAN_TYPE)
+    #     self.assertEqual(type(cfg.alpha_rural), TestReadConfig.ALPHA_RURAL_TYPE)
+    #     self.assertEqual(type(cfg.beta_urban), TestReadConfig.BETA_URBAN_TYPE)
+    #     self.assertEqual(type(cfg.beta_rural), TestReadConfig.BETA_RURAL_TYPE)
+
+
+if __name__ == '__main__':
+    unittest.main()
