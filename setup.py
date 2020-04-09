f6f5fcd1189eb8e6f07836f61c1eb48d72c6658a
crvernon <chrisrvernon@gmail.com>
2020-04-09 14:46:02 -0400

    add sample setup file

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""Setup file for <your model name>
+
+@author   <developer names>
+@email:   <developer emails>
+
+License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
+
+"""
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def get_requirements():
+    with open('requirements.txt') as f:
+        return f.read().split()
+
+
+setup(
+    name='im3py',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/IMMM-SFA/im3py.git',
+    license='BSD 2-Clause',
+    author='Chris R. Vernon',
+    author_email='chris.vernon@pnnl.gov',
+    description='A template Python model for IM3.',
+    python_requires='>=3.3.*, <4'
+)
