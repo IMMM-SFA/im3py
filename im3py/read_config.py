19d200b6debbae0a14dc3c25b8d13e062477e3a4
crvernon <chrisrvernon@gmail.com>
2020-04-09 16:02:22 -0400

    update doc string

--- im3py/read_config.py
+++ im3py/read_config.py
@@ -1,7 +1,7 @@
 """Configuration reader for <your model name>
 
-@author   <developer names>
-@email:   <developer emails>
+:author:   <developer names>
+:email:    <developer emails>
 
 License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
 
@@ -38,28 +38,28 @@ class ReadConfig:
                                                 population that is located within the 100 km neighborhood, the more
                                                 suitable the focal cell is.  More negative value implies less suitable.
                                                 Acceptable range:  -2.0 to 2.0
-    :type alpha_urban:                          int
+    :type alpha_urban:                          float
 
 
-    :param beta_urban:                          float. Beta parameter for urban. Reflects the significance of distance
+    :param beta_urban:                          Beta parameter for urban. Reflects the significance of distance
                                                 to surrounding cells on the suitability of a focal cell.  Within 100 km,
                                                 beta determines how distance modifies the effect on suitability.
                                                 Acceptable range:  -0.5 to 2.0
-    :type beta_urban:                           int
+    :type beta_urban:                           float
 
-    :param alpha_rural:                         float. Alpha parameter for rural. Represents the degree to which the
+    :param alpha_rural:                         Alpha parameter for rural. Represents the degree to which the
                                                 population size of surrounding cells translates into the suitability
                                                 of a focal cell.  A positive value indicates that the larger the
                                                 population that is located within the 100 km neighborhood, the more
                                                 suitable the focal cell is.  More negative value implies less suitable.
                                                 Acceptable range:  -2.0 to 2.0
-    :type alpha_rural:                          int
+    :type alpha_rural:                          float
 
-    :param beta_rural:                          float. Beta parameter for rural. Reflects the significance of distance
+    :param beta_rural:                          Beta parameter for rural. Reflects the significance of distance
                                                 to surrounding cells on the suitability of a focal cell.  Within 100 km,
                                                 beta determines how distance modifies the effect on suitability.
                                                 Acceptable range:  -0.5 to 2.0
-    :type beta_rural:                           int
+    :type beta_rural:                           float
 
     """
 
