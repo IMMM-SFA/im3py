b8db22e373fab4815064aa75867717e8847c0f1a
crvernon <chrisrvernon@gmail.com>
2020-04-09 14:58:35 -0400

    add init file with imports

---
+++ im3py/__init__.py
@@ -0,0 +1,15 @@
+"""High level package initialization to make classes and functions within the package visible to the user.
+
+@author   <developer names>
+@email:   <developer emails>
+
+License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
+
+"""
+
+from im3py.model import Model
+from im3py.process_step import ProcessStep
+from im3py.read_config import ReadConfig
+
+
+__all__ = ['Model', 'ProcessStep', 'ReadConfig']
